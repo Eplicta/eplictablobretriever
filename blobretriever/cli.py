d340954eba4c8cd5ddfebf0dd9d8d20f18b3195a
James Frih <james.frih@gruppera.se>
2023-03-24 12:12:02 +0100

    add download blobs command

--- blobretriever/cli.py
+++ blobretriever/cli.py
@@ -18,4 +18,13 @@ def list_blobs(
     blob_list = blob_storage_client.list_blobs_from_container(container_name)
     for blob_name in blob_list:
         typer.secho(blob_name)
+
+@app.command()
+def download_blobs(
+    container_name: str = typer.Argument(...)
+) -> None:
+    config = Config()
+
+    blob_storage_client = BlobStorageClient(config.get_option('BLOB_CONFIG', 'ACCOUNT_NAME'), config.get_option('BLOB_CONFIG', 'ACCOUNT_KEY'))
+    blob_storage_client.download_blobs_from_container(container_name)
     
