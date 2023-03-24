27db198558d0bc719e0fe8704622a01ad4e5cb8f
James Frih <james.frih@gruppera.se>
2023-03-24 11:17:20 +0100

    add download blobs from container function to blobstorageclient

--- blobstorageclient.py
+++ blobstorageclient.py
@@ -5,6 +5,8 @@ from config import Config
 
 class BlobStorageClient():
     def __init__(self, account_name, account_key):
+        self.config = Config()
+
         self.account_name = account_name
         self.account_key = account_key
         self.account_url = f'https://{self.account_name}.blob.core.windows.net'
@@ -12,13 +14,21 @@ class BlobStorageClient():
         self.blob_service_client = BlobServiceClient(account_url= self.account_url, credential= self.account_key)
     
     def download_blob(self, container_name, blob_name):
-        config = Config()
-
-        download_path = os.path.join(config.get_option("APP_CONFIG", "DOWNLOAD_DIRECTORY"), blob_name)
-
+        download_path = os.path.join(self.config.get_option("APP_CONFIG", "DOWNLOAD_DIRECTORY"), blob_name)
         container_client = self.blob_service_client.get_container_client(container= container_name)
+
+        self.__download_blob(self, container_client, download_path, blob_name)
+
+    def download_blobs_from_container(self, container_name):
+        container_client = self.blob_service_client.get_container_client(container= container_name)
+        blob_list = container_client.list_blob_names()
+
+        for blob_name in blob_list:
+            download_path = os.path.join(self.config.get_option("APP_CONFIG", "DOWNLOAD_DIRECTORY"), blob_name)
+            self.__download_blob(container_client, download_path, blob_name)
+
+    def __download_blob(self, container_client, download_path, blob_name):
         print("\nDownloading blob to \n\t" + download_path)
 
         with open(download_path, "wb") as download_file:
             download_file.write(container_client.download_blob(blob_name).readall())
-            
